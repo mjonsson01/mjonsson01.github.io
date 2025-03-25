1cabd67d1090344cc40ac6beb20a1e1f8cbe7b3c
Marco Jonsson <marco.e.jonsson@gmail.com>
2025-03-24 19:33:55 -0500

    Many changes. Contents of chess page, File move around, animation of frontpage, slick behavior, fixed nav bar, imported pictures, updated contacts

--- NavBarAndSiteBehaviors/navbar-behavior.js
+++ NavBarAndSiteBehaviors/navbar-behavior.js
@@ -4,20 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(html => {
             document.getElementById("navbar-container").innerHTML = html;
-            disableCurrentPageLink(); // Call function after navbar loads
+            disableCurrentPageLink(); 
         });
 
     function disableCurrentPageLink() {
-        let currentPage = window.location.pathname.replace(/\/$/, "").split("/").pop() || "index.html"; // Normalize path
+        let currentPage = window.location.pathname.replace(/\/$/, "").split("/").pop() || "index.html"; 
+        currentPage = currentPage.split('?')[0].split('#')[0]; 
 
         document.querySelectorAll("#navbar-container nav ul li a").forEach(link => {
             let linkPage = link.getAttribute("href").replace(/\/$/, "").split("/").pop();
+            linkPage = linkPage.split('?')[0].split('#')[0]; 
 
             if (linkPage === currentPage) {
                 link.classList.add("current-page");
-                link.removeAttribute("href"); // Disable link
-                link.style.pointerEvents = "none"; // Block clicking
-                link.style.color = "gray"; // Optional styling
+                link.removeAttribute("href"); 
+                link.style.pointerEvents = "none"; 
+                link.style.color = "gray"; 
+                // link.style.textDecoration = "none"; 
+                link.style.fontWeight = "bold"; 
             }
         });
     }
